fa78883a242d8faeff34c979bcb0a74cb9efc6fc
efesultan <58030231+efesultan@users.noreply.github.com>
2026-03-01 13:24:27 +0000

    Add target_link_directories for Maya plugin

--- cmake/FindMaya.cmake
+++ cmake/FindMaya.cmake
@@ -32,7 +32,7 @@ find_package_handle_standard_args(Maya DEFAULT_MSG MAYA_DEVKIT_DIR)
 
 function(MAYA_PLUGIN target)
     target_include_directories(${target} PUBLIC ${MAYA_DEVKIT_DIR}/include)
-
+    target_link_directories(${target} PUBLIC ${MAYA_DEVKIT_DIR}/lib)
     set(MAYA_LIBS Foundation OpenMaya OpenMayaAnim OpenMayaFX OpenMayaRender OpenMayaUI clew)
     foreach(MAYA_LIB ${MAYA_LIBS})
         target_link_libraries(${target} PUBLIC ${MAYA_DEVKIT_DIR}/lib/${MAYA_LIB_PREFIX}${MAYA_LIB}${MAYA_LIB_SUFFIX})
